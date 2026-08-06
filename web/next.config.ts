7f9fa2a33b1ec44de37e2ca9332ace1ac985d148
Shah Md Mahi <shahmdmahi15@gmail.com>
2026-08-06 14:29:42 +0600

    update

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,9 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "standalone",
-  // Add the packages in transpilePackages
-  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  // output: "standalone",
+  // // Add the packages in transpilePackages
+  // transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
   images: {
     remotePatterns: [
       {
