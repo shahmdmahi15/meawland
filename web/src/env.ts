3a7d07cc3fe652a62c4c9d60d141e42e36a53364
Shah Md Mahi <shahmdmahi15@gmail.com>
2026-08-22 14:19:33 +0600

    update

--- web/src/env.ts
+++ web/src/env.ts
@@ -61,6 +61,8 @@ export const env = createEnv({
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   skipValidation:
-    !!process.env.SKIP_ENV_VALIDATION ||
+    process.env.SKIP_ENV_VALIDATION === "true" ||
+    process.env.SKIP_ENV_VALIDATION === "1" ||
     process.env.npm_lifecycle_event === "lint",
+  emptyStringAsUndefined: true,
 });
